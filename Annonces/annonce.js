6e32e1d7eece3acb23059de24a36b9434bce2598
youssefthetunisianboy4456 <ajmiy72@gmail.com>
2026-08-31 00:51:51 +0100

    Make annonces nice and add meta

--- Annonces/annonce.js
+++ Annonces/annonce.js
@@ -3,31 +3,14 @@ import { loadAnnonces } from "./annonceList.js";
 export function fillAnnonces(annoncesToDisplay) {
   return annoncesToDisplay
     .map(annonce => `
-      <div class="annonce annonce${annonce.id}">
-        <div>
-          <img
-            src="../Images/${annonce.image}"
-            class="annonce-image"
-          >
-        </div>
-
-        <div class="annonce-titre">
-          ${annonce.mainText}
-        </div>
-
-        <div class="annonce-main-text">
-          ${annonce.subText}
-        </div>
-
-        <div class="savoir-plus savoir-plus-${annonce.id}">
-          <a
-            class="special-link"
-            href="../Annonces/singleAnnonce/singleAnnonce.html?id=${annonce.id}"
-          >
-            En savoir plus
-          </a>
-        </div>
-      </div>
+      <article class="annonce">
+					<img class="annonce-image" src="../Images/${annonce.image}">
+					<div class="annonce-text">
+						<h3>${annonce.mainText}</h3>
+						<p>${annonce.subText}</p>
+						<a href="./singleAnnonce/singleAnnonce.html?id=${annonce.id}">En savoir plus</a>
+					</div>
+			</article>
     `)
     .join("");
 }
