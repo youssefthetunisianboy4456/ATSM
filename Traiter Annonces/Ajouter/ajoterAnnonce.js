fadef5a4df5773556d7ac3d13ecbe33e64bb4bc0
youssefthetunisianboy4456 <ajmiy72@gmail.com>
2026-09-05 05:38:30 +0100

    fix ajoterAnnonce.js

--- Traiter Annonces/Ajouter/ajoterAnnonce.js
+++ Traiter Annonces/Ajouter/ajoterAnnonce.js
@@ -1,24 +1,37 @@
 /*import {latexText} from './latexText.js'*/
-const annonces=JSON.parse(localStorage.getItem('annonces')) || []
 /*latexText('Latex','fill');*/
-    function fillTextOfFrom(pClass,inputId) {
+function fillTextOfFrom(pClass,inputId) {
     setInterval( function f1() {
     document.querySelector(`.${pClass}`).innerHTML=document.querySelector(`.${inputId}`).value;
 },200);
 }
 fillTextOfFrom('Preview','fill');
-document.querySelector('.add').addEventListener('click', function () {
+document.querySelector('.add').addEventListener('click', async function () {
     let x=[]
-    for (let i=0 ; i<5 ; i++) {
+    for (let i=0 ; i<7 ; i++) {
         x[i]=document.querySelector(`.c${i}`).value
     }
-    annonces.unshift( {
+    const newAnnonce = {
         id:x[0],
         mainText : x[1],
         image :x[2],
         subText :x[3],
-        pressReport:x[4]
-    })
-    console.log(annonces)
-    localStorage.setItem('annonces',JSON.stringify(annonces))
+        pressReport:x[4],
+        date : x[5],
+        type: x[6]
+    }
+    const token = sessionStorage.getItem('adminToken');
+    await fetch(
+        'https://atsm-backend.onrender.com/add',
+        {
+            method: 'POST',
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+
+            body: newAnnonce
+        }
+    );
 })
