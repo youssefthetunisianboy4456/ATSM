ce812b878807f950cfdc27a1983cbd97c9c1d043
youssefthetunisianboy4456 <ajmiy72@gmail.com>
2026-09-05 05:55:17 +0100

    put add in html

--- Traiter Annonces/Ajouter/ajoterAnnonce.js
+++ Traiter Annonces/Ajouter/ajoterAnnonce.js
@@ -6,32 +6,3 @@ function fillTextOfFrom(pClass,inputId) {
 },200);
 }
 fillTextOfFrom('Preview','fill');
-async function add() {
-    let x=[]
-    for (let i=0 ; i<7 ; i++) {
-        x[i]=document.querySelector(`.c${i}`).value
-    }
-    const newAnnonce = {
-        id:x[0],
-        mainText : x[1],
-        image :x[2],
-        subText :x[3],
-        pressReport:x[4],
-        date : x[5],
-        type: x[6]
-    }
-    const token = sessionStorage.getItem('adminToken');
-    await fetch(
-        'https://atsm-backend.onrender.com/add',
-        {
-            method: 'POST',
-
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-
-            body: JSON.stringify(newAnnonce)
-        }
-    );
-}
