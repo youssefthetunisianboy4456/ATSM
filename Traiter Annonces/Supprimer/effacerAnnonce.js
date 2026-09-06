f1b889583928d59330e6b3f50940caceaaeb96bb
youssefthetunisianboy4456 <ajmiy72@gmail.com>
2026-09-06 01:18:36 +0100

    stupid mistake

--- Traiter Annonces/Supprimer/effacerAnnonce.js
+++ Traiter Annonces/Supprimer/effacerAnnonce.js
@@ -1,3 +1,3 @@
 async function deleteAnnonce() {
-        await fetch(`https://atsm-backend.onrender.com/delete/:${document.querySelector('.c0').value}`, { method: 'DELETE' });
+        await fetch(`https://atsm-backend.onrender.com/delete/${document.querySelector('.c0').value}`, { method: 'DELETE' });
 }
