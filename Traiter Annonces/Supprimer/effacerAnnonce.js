9180b8a7e864beeaad45f7dcf0bea571195fdba8
youssefthetunisianboy4456 <ajmiy72@gmail.com>
2026-09-06 21:22:27 +0100

    fix reload

--- Traiter Annonces/Supprimer/effacerAnnonce.js
+++ Traiter Annonces/Supprimer/effacerAnnonce.js
@@ -2,7 +2,6 @@ async function deleteAnnonce() {
         const token=sessionStorage.getItem('adminToken')
         const response = await fetch(`https://atsm-backend.onrender.com/delete/${document.querySelector('.c0').value}`, 
         { method: 'DELETE' ,headers: { 'Authorization': `Bearer ${token}`} });
-        location.reload()
         const result = await response.json()
         location.reload()
 }
