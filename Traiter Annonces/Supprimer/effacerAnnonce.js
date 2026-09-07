cd2cc7879c8d70e1830470da2c837f08c4a51ab2
youssefthetunisianboy4456 <ajmiy72@gmail.com>
2026-09-07 05:20:50 +0100

    style

--- Traiter Annonces/Supprimer/effacerAnnonce.js
+++ Traiter Annonces/Supprimer/effacerAnnonce.js
@@ -1,10 +1,6 @@
 function closePopup(){
     document.getElementById('popup').style.display="none";
 }
-function openPopup(){
-    document.getElementById('popup').style.display="flex";
-    console.log('hello')
-}
 async function deleteAnnonce() {
         const token=sessionStorage.getItem('adminToken')
         const response = await fetch(`https://atsm-backend.onrender.com/delete/${document.querySelector('.c0').value}`, 
