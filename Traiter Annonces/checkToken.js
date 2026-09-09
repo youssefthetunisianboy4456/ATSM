dd022f0ee0dc24f46db101ae5f4d1852244396ca
youssefthetunisianboy4456 <ajmiy72@gmail.com>
2026-09-09 04:37:18 +0100

    delete supp and modif and several fixes

--- Traiter Annonces/checkToken.js
+++ Traiter Annonces/checkToken.js
@@ -1,4 +1,7 @@
+document.body.style.visibility="hidden"
 const token = sessionStorage.getItem('adminToken');
 if (!token) {
     window.location.href = '../login.html';
+} else {
+    document.body.style.visibility="hidden"
 }
